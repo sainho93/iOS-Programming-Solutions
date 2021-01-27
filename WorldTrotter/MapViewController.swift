ad0c13059b0ed26a1094d10463a714fad11d9604
sainho <taozhongimd@gmail.com>
2021-01-27 15:56:44 +0100

    Add view controllers into a tab view controller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by sainho on 27.01.21.
+//  Copyright © 2021 Big Nerd Ranch. All rights reserved.
+//
+
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("MapViewController loaded its view.")
+    }
+
+}
