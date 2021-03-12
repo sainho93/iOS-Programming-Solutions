495606dc20274665eaaa7d14ba029605785871fc
sainho <taozhongimd@gmail.com>
2021-03-12 16:13:23 +0100

    Finish Long Item Names

--- 10-TableViewCells/LootLogger/LootLogger/Item.swift
+++ 10-TableViewCells/LootLogger/LootLogger/Item.swift
@@ -27,7 +27,9 @@ class Item: Equatable {
     convenience init(random: Bool = false) {
         if random {
             let adjectives = ["Fluffy", "Rusty", "Shiny"]
-            let nouns = ["Bear", "Spork", "Mac"]
+            let nouns = ["BearBearBearBearBearBearBearBearBearBear",
+                         "SporkSporkSporkSporkSporkSporkSporkSpork",
+                         "MacMacMacMacMacMacMacMacMacMacMacMacMacMac"]
             
             let randomAdjective = adjectives.randomElement()!
             let randomNoun = nouns.randomElement()!
